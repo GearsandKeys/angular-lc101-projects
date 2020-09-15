800d70bcbf7b4bd57711e4532f4acfe02e241055
Nathan Gearke <n.gearke@gmail.com>
2020-09-14 21:31:42 -0500

    working on the bonus

--- lesson3/examples/src/app/buttons/buttons.component.ts
+++ lesson3/examples/src/app/buttons/buttons.component.ts
@@ -7,10 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ButtonsComponent implements OnInit {
    buttonHeading: string = "Buttons"
-   inactive: boolean = false;
+   inactiveGold: boolean = false;
+   inactiveSilver: boolean = false;
+   inactiveCopper: boolean = false;
+   activeFrustration: boolean= true;
 
    constructor() { }
 
    ngOnInit() { }
 
+   activateTheButtons() {
+      this.inactiveGold = false;
+      this.inactiveSilver = false;
+      this.inactiveCopper = false;
+   }
 }
+/*
+   Book Example for the buttons all turning back on, each of the buttons has [class.gold]="buttonState[0]" etc
+   buttonHeading: string = "Buttons"
+   active: boolean = true;
+   buttonState: boolean[] = [true, true, true];
+
+   reactivateButtons() {
+      for (let i = 0; i < this.buttonState.length; i++) {
+         this.buttonState[i] = true;
+      }
+      return this.buttonState;
+   }
+   */
+
